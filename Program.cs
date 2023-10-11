3d8c83dbded2b894821aaeb8ce15cfd111770ea4
Siliciumo2 <147613900+siliciumo2@users.noreply.github.com>
2023-10-11 14:36:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Ввести длину ребра куба");
+double x = Convert.ToDouble(Console.ReadLine());
+Double y = x * x;
+Console.WriteLine($"Площадь грани={y}");
+double z = y * 6;
+Console.WriteLine($"Площадь полной поверхности куба={z}");
+double w = x*x*x;
+Console.WriteLine($"Объем куба={w}");
